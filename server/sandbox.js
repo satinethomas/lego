0531570f77332c155a600ed198704f7f40278e55
satinethomas <satine.thomas@edu.devinci.fr>
2025-02-03 13:10:24 +0100

    creation autres fichiers

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,13 +1,32 @@
 /* eslint-disable no-console, no-process-exit */
 const avenuedelabrique = require('./websites/avenuedelabrique');
+const dealabs = require('./websites/dealabs');
 
-async function sandbox (website = 'https://www.avenuedelabrique.com/nouveautes-lego') {
+async function sandbox(website, url) {
   try {
-    console.log(`🕵️‍♀️  browsing ${website} website`);
+    if (!website || !url) {
+      console.log('You must provide both a website and an URL.');
+      process.exit(1);
+    }
 
-    const deals = await avenuedelabrique.scrape(website);
+    console.log(`browsing ${website} website`);
 
-    console.log(deals);
+    let deals;
+
+    // Switch pour choisir le site en fonction de l'argument 'website'
+    switch (website.toLowerCase()) {
+      case 'avenuedelabrique':
+        deals = await avenuedelabrique.scrape(url);
+        break;
+      case 'dealabs':
+        deals = await dealabs.scrape(url);
+        break;
+      default:
+        console.log('Website not recognized. Please choose "avenuedelabrique" or "dealabs".');
+        process.exit(1);
+    }
+
+    console.log(deals); // Affiche les résultats
     console.log('done');
     process.exit(0);
   } catch (e) {
@@ -16,6 +35,9 @@ async function sandbox (website = 'https://www.avenuedelabrique.com/nouveautes-l
   }
 }
 
-const [,, eshop] = process.argv;
+// Récupère les arguments de la ligne de commande
+const [,, eshop, url] = process.argv;
 
-sandbox(eshop);
+// Appelle la fonction sandbox avec le nom du site et l'URL en argument
+sandbox(eshop, url);
+
--- server/websites/dealabs.js
+++ server/websites/dealabs.js
@@ -13,48 +13,54 @@ const parse = data => { //data: chaîne de caractères contenant le HTML de la p
     .map((i, element) => { // parcourt tous les 'a' trouvés 
       const price = parseInt( //extraction du prix (convertit le prix en nombre entier)
         $(element)
-          .find('span.prodl-prix span') //là ou les prix sont rangés (repérage avant sur le site)
+          .find('text--b size--all-xl size--fromW3-xxl thread-price') //là ou les prix sont rangés (repérage avant sur le site)
           .text() //récupère le texte à l'intérieur 
       );
 
       const discount = Math.abs(parseInt( //extraction de la réduction en s'assurant que la valeur est positive
         $(element)
-          .find('span.prodl-reduc')
+          .find('textBadge bRad--a-m flex--inline text--b boxAlign-ai--all-c size--all-s size--fromW3-m space--h-1 space--ml-1 space--mr-0 textBadge--green')
           .text()
       ));
 
       // récupérer les infos de l'image
-      const imgRef =  'https://www.avenuedelabrique.com/img/'+ $(element)
-      .find('span.prodl-img img')
-      .attr('data-src');  // Récupère l'attribut src de l'image
+      //const imgRef =  'https://www.avenuedelabrique.com/img/'+ $(element)
+      //.find('span.prodl-img img')
+      //.attr('data-src');  // Récupère l'attribut src de l'image
 
       return { //extraction du titre
         discount,
         price,
         'title': $(element).attr('title'),
-        imgRef,
+        
       };
     })
     .get(); // convertit tout en tableau contenant les objets price, discount et title
 };
 
 /**
- * Scrape a given url page
- * @param {String} url - url to parse
- * @returns 
+ * Scrape a given URL page
+ * @param {String} url - URL to parse
+ * @returns {Promise<Array|null>} Extracted deals
  */
-module.exports.scrape = async url => { //Exporte la fonction scrape pour pouvoir l'utiliser dans un autre fichier
-  const response = await fetch(url); //télécharge la page web
-
-  if (response.ok) { //vérifie si la page a été téléchargée
-    const body = await response.text(); // convertit la page en texte brut (HTML)
-
-    return parse(body); //Passe le HTML téléchargé à la fonction parse, qui extrait les produits
-  }
-
-  console.error(response); 
-
-  return null;
-}; 
+module.exports.scrape = async url => {
+    try {
+      const response = await fetch(url, {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+          'Accept-Language': 'en-US,en;q=0.9',
+          'Referer': 'https://www.google.com/',
+        },
+      });
+  
+      if (!response.ok) throw new Error('HTTP error! status: ${response.status}');
+  
+      const body = await response.text();
+      return parse(body);
+    } catch (error) {
+      console.error('Error scraping ${url}:', error.message);
+      return null;
+    }
+  }; 
 
 // ensuite fichier .js va utiliser ce module (sandbox.js)
